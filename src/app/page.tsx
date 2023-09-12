f82bb973d5d872341d4d6a63c6911159df140d53
Diogo Soares <dioggo.soares@live.com>
2023-09-12 00:53:22 -0300

    feat: Create UI for app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,154 @@
-import Image from 'next/image'
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Label } from '@/components/ui/label'
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+} from '@/components/ui/select'
+import { Separator } from '@/components/ui/separator'
+import { Slider } from '@/components/ui/slider'
+import { Textarea } from '@/components/ui/textarea'
+import { FileVideo, Github, Heart, Play, Upload, Wand2 } from 'lucide-react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="flex flex-col min-h-screen p-6 bg-gradient-to-r from-yellow-600 to-red-600">
+      <div className="flex flex-1 flex-col rounded-xl bg-zinc-900">
+        <header className="flex items-center px-6 py-3 justify-between border-b overflow-hidden">
+          <div className="flex gap-3 items-center">
+            <Play className="h-6 w-6" fill="#f97316" />
+            <h1 className="text-xl font-bold">upload.ai</h1>
+          </div>
+          <div className="flex gap-3 items-center">
+            <span className="text-sm text-muted-foreground flex items-center gap-2">
+              Desenvolvido com <Heart className="h-4 w-4" fill="#dc2626" /> por
+              Diogo
+            </span>
+
+            <Separator orientation="vertical" className="h-6" />
+
+            <Button variant="outline">
+              <Github className="h-4 w-4 mr-2" />
+              Github
+            </Button>
+          </div>
+        </header>
+
+        <main className="flex-1 p-6 flex gap-6">
+          <div className="flex flex-col flex-1 gap-4">
+            <div className="grid grid-rows-2 gap-4 flex-1">
+              <Textarea
+                className="resize-none p-4 leading-relaxed"
+                placeholder="Inclua o prompt para a IA..."
+              />
+              <Textarea
+                className="resize-none p-4 leading-relaxed"
+                placeholder="Resultado gerado pela IA..."
+                readOnly
+              />
+            </div>
+            <p className="text-sm text-muted-foreground">
+              Lembre-se: você pode utilizar a variável{' '}
+              <code className="text-orange-400">{'{transcription}'}</code> no
+              seu prompt para adicionar o conteúdo da transcrição do vídeo
+              selecionado.
+            </p>
+          </div>
+
+          <aside className="w-full max-w-xs space-y-6">
+            <form className="space-y-6">
+              <label
+                htmlFor="video"
+                className="border flex rounded-md border-dashed aspect-video
+                cursor-pointer text-sm flex-col gap-2 items-center justify-center
+                text-muted-foreground hover:bg-primary/5"
+              >
+                <FileVideo className="h-4 w-4" />
+                Selecione um vídeo
+              </label>
+
+              <input
+                type="file"
+                id="video"
+                accept="video/mp4"
+                className="sr-only"
+              />
+
+              <Separator />
+
+              <div className="space-y-2">
+                <Label htmlFor="transcription_prompt">
+                  Prompt de transcrição
+                </Label>
+                <Textarea
+                  id="transcription_prompt"
+                  className="min-h-[5rem] leading-relaxed resize-none"
+                  placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
+                />
+              </div>
+
+              <Button type="submit" className="flex w-full">
+                Carregar vídeo
+                <Upload className="h-4 w-4 ml-2" />
+              </Button>
+            </form>
+
+            <Separator />
+
+            <form className="space-y-6">
+              <div className="space-y-4">
+                <Label>Prompt</Label>
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Selecione um prompt..." />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="title">Título do vídeo</SelectItem>
+                    <SelectItem value="descrition">
+                      Descrição do vídeo
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+
+              <div className="space-y-4">
+                <Label>Modelo</Label>
+                <Select disabled defaultValue="gpt3.5">
+                  <SelectTrigger>
+                    <SelectValue />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                  </SelectContent>
+                </Select>
+                <span className="block text-xs text-muted-foreground italic">
+                  Você poderá customizar essa opção em breve
+                </span>
+              </div>
+
+              <Separator />
+
+              <div className="space-y-2">
+                <Label>Temperatura</Label>
+                <Slider min={0} max={1} step={0.1} />
+                <span className="block text-xs text-muted-foreground italic leading-relaxed">
+                  Valores mais altos tendem a deixar o resultado mais criativo e
+                  com possíveis erros.
+                </span>
+              </div>
+
+              <Button type="submit" className="w-full">
+                Executar
+                <Wand2 className="h-4 w-4 ml-2" />
+              </Button>
+            </form>
+          </aside>
+        </main>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('prettier-plugin-tailwindcss')],
 }
 export default config
